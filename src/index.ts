ed8c2733744636ff38855e58a8f3f7028cb7d9b3
mfotzler <mfotzler@gmail.com>
2025-11-07 15:03:51 -0600

    Add main scene with title screen

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,27 @@
 import * as Phaser from 'phaser';
 import MessageBus from './messageBus/MessageBus';
 import getRealStorageProvider from './messageBus/realStorageProvider';
+import MainMenu from './scenes/MainMenu';
 
 MessageBus.initialize(getRealStorageProvider());
 
 new Phaser.Game({
-	type: Phaser.AUTO,
-	parent: 'game',
-	backgroundColor: '#aaaaee',
-	title: 'Minijam Template',
-	scale: {
-		width: 1920,
-		height: 1080,
-		mode: Phaser.Scale.FIT,
-		autoCenter: Phaser.Scale.CENTER_BOTH
-	},
-	scene: [],
-	physics: {
-		default: 'arcade',
-		arcade: {
-			gravity: { x: 0, y: 0 },
-			debug: false
-		}
-	}
+  type: Phaser.AUTO,
+  parent: 'game',
+  backgroundColor: '#aaaaee',
+  title: 'Minijam Template',
+  scale: {
+    width: 1280,
+    height: 720,
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH
+  },
+  scene: [MainMenu],
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { x: 0, y: 0 },
+      debug: false
+    }
+  }
 });
