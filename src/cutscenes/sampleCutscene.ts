7628b1b8a0a6117590c55e5c2263f44dfa617f86
mfotzler <mfotzler@gmail.com>
2025-11-08 11:23:28 -0600

    First pass of intro cutscene

--- src/cutscenes/sampleCutscene.ts
+++ src/cutscenes/sampleCutscene.ts
@@ -1,26 +1,20 @@
 import { Cutscene } from './types';
 
 const sampleCutscene: Cutscene = {
-  segments: [
-    {
-      imageKey: 'segfault-squad',
-      textChunks: [
-        'In the beginning, there was recursion.',
-        'It called itself, again and again.'
-      ],
-      imageTransition: 'slide-left',
-      textTransition: 'fade'
-    },
-    {
-      imageKey: 'segfault-squad',
-      textChunks: [
-        'But beware the infinite loop...',
-        "Some say it's where legends are born."
-      ],
-      imageTransition: 'fade',
-      textTransition: 'slide-fade-right'
-    }
-  ]
+	segments: [
+		{
+			imageKey: 'segfault-squad',
+			textChunks: ['In the beginning, there was recursion.', 'It called itself, again and again.'],
+			imageTransition: 'slide-left',
+			textTransition: 'fade'
+		},
+		{
+			imageKey: 'segfault-squad',
+			textChunks: ['But beware the infinite loop...', "Some say it's where legends are born."],
+			imageTransition: 'fade',
+			textTransition: 'slide-fade-right'
+		}
+	]
 };
 
 export default sampleCutscene;
